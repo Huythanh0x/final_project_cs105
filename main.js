a1384e5731bffe0dada7295c4cd9651001671b7a
huythanh0x <huythanh0x@gmail.com>
2022-05-18 19:40:58 +0700

    add light and navigator

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import './style.css'
 import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls' 
 
 const scene = new THREE.Scene()
 
@@ -14,19 +15,34 @@ renderer.setSize(window.innerWidth, window.innerHeight)
 
 camera.position.setZ(30)
 
-// renderer.render()
-
 const geometry = new THREE.TorusGeometry(10, 3, 16, 100)
 
-const material = new THREE.MeshBasicMaterial({ color: 0xFF6347, wireframe: true })
+// const material = new THREE.MeshBasicMaterial({ color: 0xFF6347, wireframe: true })
+
+const material = new THREE.MeshStandardMaterial({ color: 0xFF6347})
+
+//! add light
+const pointLight = new THREE.PointLight(0xFFFFFF)
+pointLight.position.set(55,55,55)
+const ambientLight = new THREE.AmbientLight(0x00FF55)
+scene.add(pointLight,ambientLight)
+
+// ! add light and gravitator
+const lightHelper = new THREE.PointLightHelper(pointLight)
+const gridHelper = new THREE.GridHelper(200,50)
+scene.add(lightHelper,gridHelper)
 
 const torus = new THREE.Mesh(geometry, material)
-
 scene.add(torus)
 
+const controls = new OrbitControls(camera,renderer.domElement)
 
 function animate(){
     requestAnimationFrame(animate)
+    torus.rotation.x += 0.03
+    torus.rotation.y += 0.005
+    torus.rotation.z += 0.01
+    controls.update()
     renderer.render(scene,camera)
 }
 
